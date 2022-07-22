8a7ee345413810ab11f1ef6364a384e4f621bfa9
Ravi Shanker Kushwaha <ravishankerk48@gmail.com>
2022-07-22 12:25:46 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const { Server } = require("socket.io");
+
+const io = new Server(3000, {
+    cors: {
+        origin: "http://127.0.0.1:5500",
+    },
+});
+
+io.on("connection", (socket) => {
+    socket.emit("check", "Hello world - Everything ok");
+    socket.on("message", (obj)=>{
+        socket.broadcast.emit("sendthis",obj);
+    })
+});
